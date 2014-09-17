254b280bf6fd1ff7b8393bda0ff10b7378437fee
m5 <micah@qwerjk.com>
2014-09-17 12:39:18 -0400

    put encodeURIComponent back
    The #'s in color values wreck things otherwise

--- public/js/graph.js
+++ public/js/graph.js
@@ -111,10 +111,10 @@ Graphiti.Graph.prototype = {
     var timeFrameOptions = Graphiti.timeFrameOptions();
     var opts = $.extend({}, this.options, timeFrameOptions);
     $.each(opts, function(key,value){
-      parts.push(key + "=" + value);
+      parts.push(key + "=" + encodeURIComponent(value));
     });
     $.each(this.parsedTargets, function(c, target){
-      parts.push("target=" + target);
+      parts.push("target=" + encodeURIComponent(target));
     });
     parts.push('_timestamp_=' + new Date().getTime());
     return url + parts.join('&') + '#.png';
